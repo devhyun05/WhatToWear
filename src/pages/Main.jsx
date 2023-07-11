cad1ebbf50f739f1f98bf2b4515be48215bb4b53
HyunSeongLee <devhyun05@gmail.com>
2023-07-11 16:30:44 -0400

    search icon, remove icon updated

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,43 @@
+import { useState, useEffect } from 'react'; 
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import ClearIcon from '@mui/icons-material/Clear';
 import TextField from '@mui/material/TextField';
 import '../App.css'; 
 
 const Main = () => {
+    const [keyword, setKeyword] = useState(''); 
+    const [searchValue, setSearchValue] = useState(''); 
+
+    const handleChange = (event) => {
+        setSearchValue(event.target.value); 
+    };
+
     return (
         <div>
             <div className="search-box-wrapper">
                 <h2>Search Your Location</h2>
-                <TextField variant="outlined" InputProps={{style:{width: '700px', borderRadius: '20px'}}}/>
+   
+                <TextField variant="outlined" 
+                value={searchValue}
+                onChange={handleChange}
+                InputProps={{
+                    startAdornment: (
+                        <IconButton>
+                            <SearchIcon/>
+                        </IconButton>
+                    ),
+                    endAdornment: (
+                        <IconButton>
+                            <ClearIcon onClick={() => setSearchValue("")}/>
+                        </IconButton>
+                    ),
+                    style:{
+                        width: '700px', 
+                        borderRadius: '20px'
+                    }}}>
+                    
+                </TextField>
             </div>
         </div>
     );
