9c6c711df448646185e4a5fbe4bfe72162289ce6
HyunSeongLee <devhyun05@gmail.com>
2023-09-14 17:46:32 -0400

    app to heroku

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -10,7 +10,7 @@ import Collapse from '@mui/material/Collapse';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import '../App.css'; 
 
-const backend = 'http://localhost:8000'; 
+const backend = 'https://weatherclothesrecommendation-f8e0423120e4.herokuapp.com/'; 
 
 const theme = createTheme({
     components: {
