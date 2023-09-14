5f0f2dff996538ac2f48538e6a7a41b7c5dc07ca
HyunSeongLee <devhyun05@gmail.com>
2023-09-14 01:27:22 -0400

    items descriptions updated

--- src/pages/Outfit.jsx
+++ src/pages/Outfit.jsx
@@ -76,23 +76,23 @@ const Outfit = () => {
             outfit = (
                 <>
                   <OutfitImage src={process.env.PUBLIC_URL + '/img/t-shirt.png'} alt="T-shirt" description="T-shirt" />
-                  <OutfitImage src={process.env.PUBLIC_URL + '/img/short_pants.png'} alt="Short pants" description="Short pants" />
-                  <OutfitImage src={process.env.PUBLIC_URL + '/img/canvas_shoes.png'} alt="Canvas shoes" description="Canvas shoes" />
+                  <OutfitImage src={process.env.PUBLIC_URL + '/img/short-pants.png'} alt="Short pants" description="Short pants" />
+                  <OutfitImage src={process.env.PUBLIC_URL + '/img/sandle.png'} alt="Canvas shoes" description="Canvas shoes" />
                 </>
             )
         } else if (tempCelsius >= 23 && tempCelsius <= 27 ) {
             outfit = (
               <>
-                  <OutfitImage src={process.env.PUBLIC_URL + '/img/t-shirt.png'} alt="T-shirt" description="T-shirt" />
-                  <OutfitImage src={process.env.PUBLIC_URL + '/img/short_pants.png'} alt="Short pants" description="Short pants" />
-                  <OutfitImage src={process.env.PUBLIC_URL + '/img/canvas_shoes.png'} alt="Canvas shoes" description="Canvas shoes" />
+                  <OutfitImage src={process.env.PUBLIC_URL + '/img/short-dress-shirt.png'} alt="T-shirt" description="T-shirt" />
+                  <OutfitImage src={process.env.PUBLIC_URL + '/img/short-pants.png'} alt="Short pants" description="Short pants" />
+                  <OutfitImage src={process.env.PUBLIC_URL + '/img/sneakers.png'} alt="Canvas shoes" description="Canvas shoes" />
               </>
             );
         } else if (tempCelsius >= 20 && tempCelsius <= 22) {
             outfit = (
               <>
-                <OutfitImage src={process.env.PUBLIC_URL + '/img/long_shirt.png'} alt="Long shirt" description="Long shirt"/> 
-                <OutfitImage src={process.env.PUBLIC_URL + '/img/long_pants.png'} alt="Long pants" description="Long pants"/> 
+                <OutfitImage src={process.env.PUBLIC_URL + '/img/blouse.png'} alt="Long shirt" description="Long shirt"/> 
+                <OutfitImage src={process.env.PUBLIC_URL + '/img/trouser.png'} alt="Long pants" description="Long pants"/> 
                 <OutfitImage src={process.env.PUBLIC_URL + '/img/canvas_shoes.png'} alt="Canvas shoes" description="Canvas shoes" />
               </>
             );
@@ -100,32 +100,40 @@ const Outfit = () => {
             outfit =(
                 <>
                     <OutfitImage src={process.env.PUBLIC_URL + '/img/long-T-shirt.png'} alt="Sweater" description="Sweater"/> 
-                    <OutfitImage src={process.env.PUBLIC_URL + '/img/fleece-lined-pants.png'} alt="Long pants" description="Long pants"/> 
-                    <OutfitImage src={process.env.PUBLIC_URL + '/img/ankle-boots.png'} alt="Canvas shoes" description="Canvas shoes" />
+                    <OutfitImage src={process.env.PUBLIC_URL + '/img/trouser.png'} alt="Long pants" description="Long pants"/> 
+                    <OutfitImage src={process.env.PUBLIC_URL + '/img/running-shoes.png'} alt="Canvas shoes" description="Canvas shoes" />
                 </>
             )
         } else if (tempCelsius >= 12 && tempCelsius <= 16) {
             outfit = (
                 <>
                     <OutfitImage src={process.env.PUBLIC_URL + '/img/jacket.png'} alt="Sweater" description="Sweater"/> 
-                    <OutfitImage src={process.env.PUBLIC_URL + '/img/fleece-lined-pants.png'} alt="Long pants" description="Long pants"/> 
-                    <OutfitImage src={process.env.PUBLIC_URL + '/img/sneakers.png'} alt="Canvas shoes" description="Canvas shoes" />
+                    <OutfitImage src={process.env.PUBLIC_URL + '/img/jeans.png'} alt="Long pants" description="Long pants"/> 
+                    <OutfitImage src={process.env.PUBLIC_URL + '/img/running-shoes.png'} alt="Canvas shoes" description="Canvas shoes" />
                 </>
             )
         } else if (tempCelsius >= 9 && tempCelsius <= 11) {
             outfit = (
                 <>
-                    <OutfitImage src={process.env.PUBLIC_URL + '/img/coat.jpg'} alt="Trench Coat" description="Trench Coat" /> 
+                    <OutfitImage src={process.env.PUBLIC_URL + '/img/jacket.png'} alt="Trench Coat" description="Trench Coat" /> 
+                    <OutfitImage src={process.env.PUBLIC_URL + '/img/fleece-lined-pants.png'} alt="Trench Coat" description="Trench Coat" /> 
+                    <OutfitImage src={process.env.PUBLIC_URL + '/img/ankle-boots.png'} alt="Trench Coat" description="Trench Coat" /> 
                 </>
             )
         } else if (tempCelsius >= 5 && tempCelsius <= 8) {
             outfit = (
                 <>
+                    <OutfitImage src={process.env.PUBLIC_URL + '/img/padded-jacket.png'} alt="Trench Coat" description="Trench Coat" /> 
+                    <OutfitImage src={process.env.PUBLIC_URL + '/img/fleece-lined-pants.png'} alt="Trench Coat" description="Trench Coat" /> 
+                    <OutfitImage src={process.env.PUBLIC_URL + '/img/ankle-boots.png'} alt="Trench Coat" description="Trench Coat" /> 
                 </>
             )
         } else {
             outfit = (
                 <>
+                    <OutfitImage src={process.env.PUBLIC_URL + '/img/padded-jacket.png'} alt="Trench Coat" description="Trench Coat" /> 
+                    <OutfitImage src={process.env.PUBLIC_URL + '/img/fleece-lined-pants.png'} alt="Trench Coat" description="Trench Coat" /> 
+                    <OutfitImage src={process.env.PUBLIC_URL + '/img/ankle-boots.png'} alt="Trench Coat" description="Trench Coat" /> 
                 </>
             )
         }
